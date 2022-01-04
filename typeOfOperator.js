5ab7168f84759e6259a204b475429709f85b67d0
Md. Shafiul Islam Sohan <sohan110111@gmail.com>
2022-01-04 21:48:56 +0600

    Mainly Sting type of operator check

---
+++ typeOfOperator.js
@@ -0,0 +1,13 @@
+let number1 = 10;
+let number2 = 22;
+
+let text = "Hello";
+
+let concatanate_1= number1 + number2;
+
+let concatanate_2 = number1 + "text";
+
+console.log(concatanate_1);
+console.log(concatanate_2);
+console.log(typeof concatanate_1); // number
+console.log(typeof concatanate_2); // string : for priority of string
