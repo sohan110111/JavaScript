c64ffd135620ffe319d3c97c2c80722820f496a1
Md. Shafiul Islam Sohan <sohan110111@gmail.com>
2022-01-06 14:44:44 +0600

    triple equal for heck value and type

--- typeOfOperator.js
+++ typeOfOperator.js
@@ -11,4 +11,13 @@ console.log(concatanate_1);
 console.log(concatanate_2);
 console.log(typeof concatanate_1); // number
 console.log(typeof concatanate_2); // string : for priority of string
-document.getElementById("demo2").innerHTML = concatanate_1;
+//document.getElementById("demo2").innerHTML = concatanate_1;
+
+let lenghtTest = "1\t2";
+console.log(lenghtTest.length);
+document.getElementById("demo2").innerHTML = lenghtTest;
+
+let m = new Number(5);
+let n = 5;
+// now check value and type
+console.log(m === n); // false
