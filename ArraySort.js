b33ca5bf359bcf1647efaa6c27257a90dd6458e9
Md. Shafiul Islam Sohan <sohan110111@gmail.com>
2022-01-24 01:10:59 +0600

    ArraySort.js

---
+++ ArraySort.js
@@ -0,0 +1,7 @@
+const fruits = ["Apple", "Mango", "Banana", "Orange", "Jack-fruit"];
+console.log(fruits);
+document.write(fruits);
+document.write("<br>");
+fruits.sort();
+console.log(fruits);
+document.write(fruits)
