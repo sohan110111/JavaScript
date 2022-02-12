c6b9f1d1c0689e996de69c03be4e7f6edb03a473
Md. Shafiul Islam Sohan <sohan110111@gmail.com>
2022-02-12 10:55:04 +0600

    Array forEach()

---
+++ ArrayIteration.js
@@ -0,0 +1,10 @@
+const number = [45, 4, 9, 16, 25];
+
+function myFunction(value, index, array) {
+    console.log(value);
+    console.log(index);
+    console.log(array);
+    console.log("______");
+}
+
+number.forEach(myFunction);
