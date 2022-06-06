56b40c98d34b14dbabb74f8e24e779bb5fc41efe
Md. Shafiul Islam Sohan <sohan110111@gmail.com>
2022-06-06 21:29:50 +0600

    empty object practice

--- mutable_immutable_concept.js
+++ mutable_immutable_concept.js
@@ -1,11 +1,22 @@
 const person = {
     name: 'John',
-    age: 34
+    age: 34,
+    getName : function() {
+        return 'John';
+    }
 }
 
 person.name = 'Akash' // mutable concept
 console.log(person);
+console.log(person.getName());
 
 let number = 2;
 number = 78; // this is for immutable concept.
 console.log(number)
+
+// empty javaScript object
+const randomProperties = {}; 
+randomProperties.name = "Akash"
+randomProperties.Function = function () { return "Daraz"}
+console.log(randomProperties.Function());
+console.log(randomProperties)
