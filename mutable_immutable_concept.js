3db4b52d1a9d140ee8a681cf0221bab149a8fe51
Md. Shafiul Islam Sohan <sohan110111@gmail.com>
2022-06-06 11:48:47 +0600

    mutable and immutable concept

---
+++ mutable_immutable_concept.js
@@ -0,0 +1,11 @@
+const person = {
+    name: 'John',
+    age: 34
+}
+
+person.name = 'Akash' // mutable concept
+console.log(person);
+
+let number = 2;
+number = 78; // this is for immutable concept.
+console.log(number)
