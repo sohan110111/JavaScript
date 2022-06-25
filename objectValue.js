e982006e65826dbb97a12440b0fb193e277240ba
Md. Shafiul Islam Sohan <sohan110111@gmail.com>
2022-06-25 10:08:49 +0600

    get object property with for in and for of

--- objectValue.js
+++ objectValue.js
@@ -7,3 +7,17 @@ const person = {
 }
 
 console.log(Object.values(person))
+
+const personArray = Object.values(person)
+personArray.forEach(element => {
+    console.log(element)
+});
+
+for (x in personArray) {
+    //console.log(personArray[x])
+    console.log(x)
+}
+for (x of personArray) {
+    //console.log(personArray[x])
+    console.log(x)
+}
