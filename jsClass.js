f774cc5eb5d5b91d388efd4bed805f86a38e298e
Binary01 <binary01.dev@gmail.com>
2022-07-27 19:03:15 +0600

    jsClass

---
+++ jsClass.js
@@ -0,0 +1,10 @@
+class cars {
+    constructor(BrandName, Price) {
+        this.BrandName = BrandName;
+        this.Price = Price;
+    }
+}
+
+const car_1 = new cars("Ford", 3000);
+
+console.log(car_1.BrandName + ' ' + car_1.Price);
