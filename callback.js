22dee1a122a58400fcc17862028ba20af10b9773
Binary01 <binary01.dev@gmail.com>
2022-08-13 22:15:06 +0600

    call back with anonimous

--- callback.js
+++ callback.js
@@ -7,3 +7,12 @@ function sum(num1, num2) {
 }
 
 sum(2, 3);
+
+// call back example function
+
+function calculate(num1, num2, callback) {
+    let result = num1 + num2;
+    return callback(result); // call back
+}
+
+calculate(1, 3, function (res) { console.log(res)});
